8f65ba983c45f451dfeff4f9d0d3bf0b6e35ee27
kazzium <karlenedge3@gmail.com>
2026-06-13 13:39:05 -0400

    commited thug

---
+++ main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function App() {
+  return (
+    <div>
+      <h1>React Template</h1>
+      <p>This is a starter template for a React app using JavaScript.</p>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById('root');
+const root = ReactDOM.createRoot(rootElement);
+root.render(<App />);
